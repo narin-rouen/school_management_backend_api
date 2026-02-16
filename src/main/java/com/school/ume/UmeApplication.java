37823194fa79211a56486bf1b09381f3cb7db70a
narin-rouen <figfdhb@gmail.com>
2026-02-16 16:21:56 +0100

    Initial Commit

---
+++ src/main/java/com/school/ume/UmeApplication.java
@@ -0,0 +1,13 @@
+package com.school.ume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/ume/UmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.ume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
